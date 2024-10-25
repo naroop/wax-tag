c784e83ea839fac795a60fc3289c1c5e078ec914
naroop <nrupp815@outlook.com>
2024-10-25 09:01:32 -0500

    app init

---
+++ app/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'wax.tag',
+  appName: 'wax-tag',
+  webDir: 'dist'
+};
+
+export default config;
---
+++ app/src/views/BlankView.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="flex items-center h-[100vh] justify-center">
+    <h1>Hello World!</h1>
+  </div>
+</template>
