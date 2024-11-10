dffc1259e15e07c3352d70f5778523ddedbd6e81
naroop <nrupp815@outlook.com>
2024-11-10 10:27:13 -0600

    test preferences

--- app/src/views/ScanView.vue
+++ app/src/views/ScanView.vue
@@ -11,11 +11,27 @@
           <ion-title size="large">Scan</ion-title>
         </ion-toolbar>
       </ion-header>
+      <ion-input class="ml-4" label="Discogs API Key" label-placement="floating" v-model="text" helper-text="" />
+      <ion-button class="mt-5" expand="block" fill="outline" @click="submit">Submit</ion-button>
+      {{ fetched }}
     </ion-content>
   </ion-page>
 </template>
 
 <script setup lang="ts">
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
-import ExploreContainer from '@/components/ExploreContainer.vue';
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonInput, IonList, IonItem, IonButton } from '@ionic/vue';
+import { onMounted, ref } from 'vue';
+import { Preferences } from '@capacitor/preferences';
+
+const text = ref('');
+const fetched = ref<string | null>('');
+
+const submit = async () => {
+  await Preferences.set({ key: 'discogsKey', value: text.value });
+  fetched.value = text.value;
+};
+
+onMounted(async () => {
+  fetched.value = (await Preferences.get({ key: 'discogsKey' })).value;
+});
 </script>
