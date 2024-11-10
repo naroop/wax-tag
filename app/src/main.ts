a90771d311a23f04f037d7b130b7da7d1ec86a7c
naroop <nrupp815@outlook.com>
2024-11-09 18:18:55 -0600

    add tailwind

--- app/src/main.ts
+++ app/src/main.ts
@@ -1,8 +1,12 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
 import router from './router';
 
 import { IonicVue } from '@ionic/vue';
+
+import 'tailwindcss/base.css';
+import 'tailwindcss/components.css';
+import 'tailwindcss/utilities.css';
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/vue/css/core.css';
@@ -34,9 +38,7 @@ import '@ionic/vue/css/palettes/dark.system.css';
 /* Theme variables */
 import './theme/variables.css';
 
-const app = createApp(App)
-  .use(IonicVue)
-  .use(router);
+const app = createApp(App).use(IonicVue).use(router);
 
 router.isReady().then(() => {
   app.mount('#app');
---
+++ app/tailwind.config.js
@@ -0,0 +1,7 @@
+/** @type {import('tailwindcss').Config} */
+export const mode = 'jit';
+export const content = ['./index.html', './src/App.vue', './src/components/**/*.{html,js,ts,vue}', './src/views/**/*.{html,js,ts,vue}'];
+export const theme = {};
+export const corePlugins = {
+  preflight: false
+};
